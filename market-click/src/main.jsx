27e2567c2c4622e7e86b5032efea47b1f515e537
federico-ramirez <00009220@uca.edu.sv>
2024-12-16 23:16:50 -0600

    config: configure tailwindcss and react router

--- market-click/src/main.jsx
+++ market-click/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import  * as ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home';
+import { AppProvider } from './context/AppContext';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppProvider>
+      <RouterProvider router={router}/>
+    </AppProvider>
+  </React.StrictMode>,
 )
